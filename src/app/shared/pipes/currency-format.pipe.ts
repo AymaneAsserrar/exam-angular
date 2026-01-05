d70d52900cb7ee2a909a1abf89da3f09fd9994ff
AymaneAsserrar <aymane.asserrar@outlook.fr>
2026-01-05 10:34:06 +0100

    [UPD] bike card component upd

--- src/app/shared/pipes/currency-format.pipe.ts
+++ src/app/shared/pipes/currency-format.pipe.ts
@@ -1,14 +1,14 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform } from "@angular/core";
 
 @Pipe({
-  name: 'currencyFormat',
-  standalone: true
+  name: "currencyFormat",
+  standalone: true,
 })
 export class CurrencyFormatPipe implements PipeTransform {
-  transform(value: number, currencyCode: string = 'USD'): string {
-    return new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: currencyCode
+  transform(value: number, currencyCode: string = "USD"): string {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: currencyCode,
     }).format(value);
   }
 }
